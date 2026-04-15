abfe7326ac269c0aa8fcec68324d6a126cded89b
alessandrom10 <alemasini2000@gmail.com>
2026-04-15 23:20:47 +0200

    Added mpi initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "mfsolver.hpp"
 #include "ProblemData.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+    dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+    
     ADR::ProblemData<3, 2> data = ADR::ProblemData<3, 2>::standard_test_case();
 
     MFSolver::MatrixFreeADRSolver<3, 2> solver(data);
 
-    // solver.setup_system();
-    // solver.assemble();
+    solver.run();
 
     return 0;
 }
